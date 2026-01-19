0e66d73bd9fbd0a00b53f7d7866b8daf1054275c
loicsikidi <loic.sikidi@gmail.com>
2026-01-19 06:52:11 +0100

    feat: add beta info subcommand
    Note: print some info about the TPM

---
+++ cmd/beta/info/info.go
@@ -0,0 +1,175 @@
+package info
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/caarlos0/log"
+	"github.com/loicsikidi/attest/info"
+	"github.com/loicsikidi/tpm-trust/internal/logutil"
+	"github.com/loicsikidi/tpm-trust/internal/privilege"
+	"github.com/loicsikidi/tpm-trust/internal/tpm"
+	"github.com/spf13/cobra"
+)
+
+type options struct {
+	format  string
+	verbose bool
+}
+
+// Check validates the options.
+func (o *options) Check() error {
+	switch o.format {
+	case "text", "json":
+		return nil
+	default:
+		return fmt.Errorf("unsupported format %q (supported: text, json)", o.format)
+	}
+}
+
+func NewCommand() *cobra.Command {
+	opts := &options{}
+
+	cmd := &cobra.Command{
+		Use:   "info",
+		Short: "display TPM information",
+		Long: `Display static information about the TPM including vendor, manufacturer,
+firmware version, supported algorithms, and more.`,
+		Example: `  # Display TPM info in text format
+  tpm-trust beta info
+
+  # Display TPM info in JSON format
+  tpm-trust beta info --format json
+
+  # Display TPM info with verbose logging
+  tpm-trust beta info --verbose`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return run(cmd.Context(), opts)
+		},
+		Args:          cobra.NoArgs,
+		SilenceUsage:  true,
+		SilenceErrors: true,
+	}
+
+	cmd.Flags().StringVarP(&opts.format, "format", "f", "text", "Output format (text or json)")
+	cmd.Flags().BoolVarP(&opts.verbose, "verbose", "v", false, "Enable verbose logging")
+
+	return cmd
+}
+
+func run(_ context.Context, opts *options) error {
+	if err := opts.Check(); err != nil {
+		return err
+	}
+
+	// In JSON mode, we want to suppress logs to keep output clean
+	var logger *log.Logger
+	if opts.format == "json" {
+		logger = log.New(os.Stderr)
+		logger.Level = log.ErrorLevel
+	} else {
+		logger = log.New(os.Stdout)
+		if opts.verbose {
+			logger.Level = log.DebugLevel
+		}
+	}
+
+	if err := privilege.Elevate(); err != nil {
+		return fmt.Errorf("failed to elevate privileges: %w", err)
+	}
+
+	startRead := time.Now()
+	logger.Info("Reading TPM information")
+
+	tpmInfo, err := tpm.Info(tpm.TPMConfig{Logger: logger})
+	if err != nil {
+		return fmt.Errorf("failed to read TPM info: %w", err)
+	}
+
+	logutil.LogDurationWithPadding(logger, startRead)
+
+	switch opts.format {
+	case "json":
+		return outputJSON(tpmInfo)
+	default: // text
+		return outputText(logger, tpmInfo)
+	}
+}
+
+func outputJSON(tpmInfo *info.TPMInfo) error {
+	encoder := json.NewEncoder(os.Stdout)
+	encoder.SetIndent("", "  ")
+	if err := encoder.Encode(tpmInfo); err != nil {
+		return fmt.Errorf("failed to encode TPM info as JSON: %w", err)
+	}
+	return nil
+}
+
+func outputText(logger *log.Logger, tpmInfo *info.TPMInfo) error {
+	logger.Info("TPM Information")
+	logutil.LogWithPadding(logger, func() {
+		logger.WithField("vendor", tpmInfo.Vendor).Info("Vendor")
+		logger.WithField("id", tpmInfo.Manufacturer.ASCII).
+			WithField("name", tpmInfo.Manufacturer.Name).
+			Info("Manufacturer")
+		logger.WithField("revision", tpmInfo.Revision).Info("Revision")
+		logger.WithField("major", tpmInfo.FirmwareVersion.Major).
+			WithField("minor", tpmInfo.FirmwareVersion.Minor).
+			Info("Firmware Version")
+
+		if len(tpmInfo.Algorithms) > 0 {
+			logger.Infof("Supported Algorithms (%d):", len(tpmInfo.Algorithms))
+			logutil.LogWithPadding(logger, func() {
+				for _, alg := range tpmInfo.Algorithms {
+					logger.Info(alg.String())
+				}
+			})
+		}
+
+		if len(tpmInfo.KeyTypes) > 0 {
+			logger.Infof("Supported Key Types (%d):", len(tpmInfo.KeyTypes))
+			logutil.LogWithPadding(logger, func() {
+				for _, kty := range tpmInfo.KeyTypes {
+					logger.Info(kty.String())
+				}
+			})
+		}
+
+		if tpmInfo.NVMaxBufferSize > 0 {
+			logger.WithField("bytes", tpmInfo.NVMaxBufferSize).Info("NV Max Buffer Size")
+		}
+
+		if tpmInfo.NVIndexMaxSize > 0 {
+			logger.WithField("bytes", tpmInfo.NVIndexMaxSize).Info("NV Index Max Size")
+		}
+
+		if len(tpmInfo.PcrBanks) > 0 {
+			logger.Infof("PCR Banks (%d):", len(tpmInfo.PcrBanks))
+			logutil.LogWithPadding(logger, func() {
+				for _, bank := range tpmInfo.PcrBanks {
+					logger.WithField("pcrs", len(bank.PCRs)).
+						Info(bank.Alg.String())
+				}
+			})
+		}
+
+		logger.WithField("present", tpmInfo.HasEKCertChains).Info("EK Certificate Chains")
+
+		if len(tpmInfo.EKCertChains) > 0 {
+			logger.Infof("EK Certificate Chains (%d certificates)", len(tpmInfo.EKCertChains))
+			logutil.LogWithPadding(logger, func() {
+				for i, cert := range tpmInfo.EKCertChains {
+					logger.
+						WithField("subject", cert.Subject).
+						WithField("issuer", cert.Issuer).
+						Infof("index %d", i)
+				}
+			})
+		}
+	})
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/caarlos0/go-version v0.2.2
 	github.com/caarlos0/log v0.5.3
 	github.com/google/go-tpm v0.9.8
-	github.com/loicsikidi/attest v0.4.0
+	github.com/loicsikidi/attest v0.4.1-0.20260119051147-094f4acd3138
 	github.com/loicsikidi/tpm-ca-certificates v0.6.1-0.20260112223017-669c34f6b452
 	github.com/spf13/cobra v1.10.2
 	golang.org/x/sys v0.39.0
@@ -61,7 +61,7 @@ require (
 	github.com/in-toto/attestation v1.1.2 // indirect
 	github.com/in-toto/in-toto-golang v0.9.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/loicsikidi/go-tpm-kit v0.2.0 // indirect
+	github.com/loicsikidi/go-tpm-kit v0.5.1-0.20260119050512-a4ae79044cfe // indirect
 	github.com/lucasb-eyer/go-colorful v1.3.0 // indirect
 	github.com/mattn/go-runewidth v0.0.19 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
